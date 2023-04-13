b206dd12c67cb9e39ef227eebfbea1cff9e5e6cd
Tin Vuksan <tin.vuksan@vuv.hr>
2023-04-13 19:42:50 +0200

    Updates

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
 window.env = {
-  "API_KEY": "App 945c9bf931ea370b1d86d8e01ef4a302-b29943ca-b359-4fb3-b200-a94232ee495f"
+  "REACT_APP_API_KEY": "App 945c9bf931ea370b1d86d8e01ef4a302-b29943ca-b359-4fb3-b200-a94232ee495f"
 };
