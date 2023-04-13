eca106cdcacf7a9ec36fd621d4de97df25ca269d
Tin Vuksan <tin.vuksan@vuv.hr>
2023-04-13 18:55:22 +0200

    Fix attempt

--- src/API/config.js
+++ src/API/config.js
@@ -5,7 +5,7 @@ const api = axios.create({
     baseURL: `https://wpedd1.api.infobip.com`,
     headers:{
       'Content-Type': 'application/json',
-      Authorization: window.env.REACT_APP_API_KEY,
+      Authorization: process.env.REACT_APP_API_KEY,
       Accept:'application/json',
       
       
