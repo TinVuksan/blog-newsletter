f34660639b9ff51109012d1880f3c422e6e49115
Tin Vuksan <tin.vuksan@vuv.hr>
2023-04-13 19:17:14 +0200

    Removed github env secret

--- src/API/config.js
+++ src/API/config.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-
+import env from 'react-dotenv';
 const api = axios.create({
     withCredentials: true,
     baseURL: `https://wpedd1.api.infobip.com`,
     headers:{
       'Content-Type': 'application/json',
-      Authorization: process.env.REACT_APP_API_KEY,
+      Authorization: env.REACT_APP_API_KEY,
       Accept:'application/json',
       
       
